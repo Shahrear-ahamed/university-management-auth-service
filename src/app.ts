26f636ebc0e5ff41a33084152dce89c635033263
Shahrear-ahamed <its.shahrear@gmail.com>
2023-05-30 20:54:01 +0600

    Setup Eslint, Prettier, Husky & lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
-// @ts-ignore
-import express, {Application, Request, Response} from 'express';
-import cors from "cors"
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
 // use cors for cors issue
 app.use(cors())
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,19 @@
-import mongoose from "mongoose"
-import app from "./app";
-import config from "./config";
-
-// env
-const PORT: number = 500;
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function bootstrap() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log("🛢 Database connected successfully")
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('🛢 Database connected successfully')
 
-        // running server
-        app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`)
-        })
-    } catch (e) {
-        console.log("Failed to connect database")
-    }
+    // running server
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (e) {
+    console.log('Failed to connect database')
+  }
 }
 
 bootstrap()
