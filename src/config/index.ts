113990804937bf6649d0625bb0c6f7c4518e55a5
Shahrear-ahamed <its.shahrear@gmail.com>
2023-06-21 15:28:26 +0600

    [DONE] add bcrypt and hash user password

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,4 +8,5 @@ export default {
   env: process.env.NODE_DEV,
   database_url: process.env.DATABASE_URL,
   default_student_pass: process.env.DEFAULT_STUDENT_PASSWORD,
+  bcrypt_salt_round: process.env.BCRYPT_SALT_ROUNT,
 };
